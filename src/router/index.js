594956146bb2532e36d8956ba3cfbbf064e6b17c
Bryan Alzamora <alzamoradiaz123@gmail.com>
2026-09-17 21:03:25 +0200

    fix: importar ProjectsView correctamente en el router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import ContactoView from '../views/ContactoView.vue'
+import ProjectsView from '../views/ProjectsView.vue' 
 
 const routes = [
   {
